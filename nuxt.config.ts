d8618f216ef499adc3d0e4454352154e7f870844
Sanya <sanyaburmantov@gmail.com>
2024-01-29 02:16:32 +0300

    [master] - большие правки

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,8 +21,17 @@ export default defineNuxtConfig({
     head: {
       title: 'Ramok-net.by',
       meta: [
-        { name: 'description', content: 'Hello'}
+        { name: 'description', content: 'Индивидуальные номерные рамки для автомобилей с уникальными дизайнами. Выберите готовый дизайн или создайте свой собственный. Качественные номерные рамки для стильного оформления вашего авто.'},
+        { name: 'og:title', property: 'og:title', content: 'Ramok-Net.by' },
+        { name: 'og:description', property: 'og:description', content: 'Индивидуальные номерные рамки для автомобилей с уникальными дизайнами.' },
+        { name: 'og:image', property: 'og:image', content: '/logo.png' },
+        { name: 'og:url', property: 'og:url', content: 'https://ramok-net.by' },  // Замените на фактический URL вашего сайта
+        { name: 'twitter:card', content: '/logo.png' },
+        { name: 'twitter:title', content: 'Ramok-Net.by' },
+        { name: 'twitter:description', content: 'Индивидуальные номерные рамки для автомобилей с уникальными дизайнами.' },
+        { name: 'twitter:image', content: '/logo.png' }
       ],
+
       link: [
         { rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
